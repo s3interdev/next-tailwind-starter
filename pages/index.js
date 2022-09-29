a138138233608c16f2e146c845462882f9f1ffcd
Michael Owuor <michael.owuor@gmail.com>
2022-09-29 08:06:45 +0300

    feat: create layout component

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,7 @@
-import Meta from '../lib/Meta';
+import Layout from '../components/layout';
 
 const Home = () => {
-	return (
-		<>
-			{/** title bar start */}
-			<Meta title="Home | Superior Software Solutions" />
-			{/** title bar end */}
-
-			<h1 className="p-8 text-center text-4xl font-bold text-indigo-800">Your settings are well configured</h1>
-		</>
-	);
+	return <Layout title="Home">Home Page</Layout>;
 };
 
 export default Home;
